b8cd93aab17feab0ac135797042f55edfd948fa4
delwarJnu11 <delwar999393@gmail.com>
2024-05-04 16:21:57 +0600

    login api created

--- controller/userLogin.js
+++ controller/userLogin.js
@@ -1,25 +1,52 @@
-const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 const userLoginController = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    if (!email) throw new Error("please provide email");
-    if (!password) throw new Error("please provide password");
-    const user = await User.findOne({ email });
-    if (!user) throw new Error("user not found!");
+    if (!email) {
+      throw new Error("Please provide email");
+    }
+    if (!password) {
+      throw new Error("Please provide password");
+    }
 
-    const checkPassword = await bcrypt.compareSync(password, user.password);
-    console.log(checkPassword);
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new Error("user not found!");
+    }
+
+    const checkPassword = bcrypt.compareSync(password, user.password);
+
     if (checkPassword) {
+      const tokenData = {
+        _id: user._id,
+        email: user.email,
+      };
+      const token = jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
+        expiresIn: "1h",
+      });
+
+      const tokenOptions = {
+        httpOnly: true,
+        secure: true,
+      };
+
+      res.cookie("token", token, tokenOptions).status(200).json({
+        message: "Login successfull",
+        token: token,
+        success: true,
+        error: false,
+      });
     } else {
-      throw new Error("please provide valid password!");
+      throw new Error("Please check Password");
     }
   } catch (error) {
-    res.json({
-      message: error.message || error,
+    res.status(500).json({
+      message: error.message || "An error occurred during login",
       success: false,
       error: true,
     });
--- index.js
+++ index.js
@@ -9,7 +9,21 @@ const app = express();
 const PORT = 8000 || process.env.PORT;
 
 //middleware
-app.use(cors());
+
+// Enable CORS for all origins
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", router);
